8f9637e300b50751f1d18b9b5f98a5099dd17a2b
SunnyBe <ndusunday@gmail.com>
2026-01-06 12:24:28 +0100

    bump sdk version to 1.0.2

--- veview-sdk/build.gradle.kts
+++ veview-sdk/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     }
     defaultConfig {
         minSdk = 24
-        version = "1.0.1.04"
+        version = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
